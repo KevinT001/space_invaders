6944ce17cd3710665ccfd90c3ac947f69941d798
Kevin Ta <kvnta@yahoo.com>
2023-02-27 14:47:38 -0700

    save for submission

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings:
+    def __init__(self): 
+        #screen settings 
+      self.screen_width = 1200 
+      self.screen_height = 800 
+      self.bg_color =  (230, 230, 230)
+
+      # Ship setting 
+      self.ship_speed = 1.5
+
+      #Bullet Settings 
+      self.bullet_speed = 1.0
+      self.bullet_width = 3 
+      self.bullet_height = 15 
+      self.bullet_color = (60, 60, 60)
